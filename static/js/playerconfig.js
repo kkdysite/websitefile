37813b21414d7e72dfe0894b6ec580b45c99240d
kkdysite <68997453+kkdysite@users.noreply.github.com>
2021-03-02 23:50:50 +0800

    Add files via upload

---
+++ static/js/playerconfig.js
@@ -0,0 +1,7 @@
+var MacPlayerConfig={};
+//参数开始
+MacPlayerConfig={"width":"100%","height":"100%","widthmob":"100%","heightmob":"100%","widthpop":"0","heightpop":"600","second":"5","prestrain":"","buffer":"","parse":"https:\/\/jx.m3u8.tv\/jiexi\/?url=","autofull":"1","showtop":"1","showlist":"1","flag":"0","colors":"000000,F6F6F6,F6F6F6,333333,666666,FFFFF,FF0000,2c2c2c,ffffff,a3a3a3,2c2c2c,adadad,adadad,48486c,fcfcfc"};
+//参数结束
+//缓存开始
+MacPlayerConfig.player_list={"tkm3u8":{"show":"\u5929\u7a7a\u4e91\uff08\u7ebf\u8def1\uff09","des":"","ps":"1","parse":"https:\/\/jx.m3u8.tv\/jiexi\/?url="},"ckm3u8":{"show":"\u6b27\u51ef\u4e91\uff08\u7ebf\u8def2\uff09","des":"","ps":"1","parse":"https:\/\/jx.m3u8.tv\/jiexi\/?url="}},MacPlayerConfig.downer_list={"http":{"show":"http\u4e0b\u8f7d","des":"des\u63d0\u793a\u4fe1\u606f","ps":"0","parse":""},"xunlei":{"show":"xunlei\u4e0b\u8f7d","des":"des\u63d0\u793a\u4fe1\u606f","ps":"0","parse":""}},MacPlayerConfig.server_list={"server1":{"show":"\u6d4b\u8bd5\u670d\u52a1\u56681","des":"des\u63d0\u793a\u4fe1\u606f1"}};
+//缓存结束
